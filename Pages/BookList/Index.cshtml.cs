1fcfeb5f3e87c1f85867b6e6746b1b4b736ec7cf
Marivaldo Sena <marivaldo.sena@live.com>
2019-04-20 23:50:54 -0300

    Incluir tabela para mostrar os livros criados.

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -2,16 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BooksList.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BooksList.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
         }
     }
 }
